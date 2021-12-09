2295f2e199094a7ae557a00541c2b0e306222df3
Jonathan Franco <jonas150397@gmail.com>
2021-12-09 14:09:16 -0500

    waitForNextUpdate

--- src/test/hooks/useFetchGifts.test.js
+++ src/test/hooks/useFetchGifts.test.js
@@ -2,10 +2,23 @@ import { useFetchGifts } from "../../hooks/useFetchGifs";
 import { renderHook } from "@testing-library/react-hooks";
 
 describe("Pruebas en el hook useFetchGifts", () => {
-  test("debe retornar el estado inicial", () => {
-    const { result } = renderHook(() => useFetchGifts("Dragon ball"));
+  test("debe retornar el estado inicial", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifts("Dragon ball")
+    );
     const { data, loading } = result.current;
+    await waitForNextUpdate();
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+
+  test("debe retonar un arreglo un de imgs y el loading en false", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifts("Dragon ball")
+    );
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
   });
 });
